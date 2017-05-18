65fcc1484df5e35b3db5f998f7765cfb6af3ebca
Oskar Rundgren <orundg@gmail.com>
2017-05-18 21:35:12 +0100

    Call tree with paths.

--- test/test.c
+++ test/test.c
@@ -1,19 +1,19 @@
-void func1(int x)
-{
-  int i = 1;
-  float x = 0.1;
-  float y = 0.2;
-  func2();
-  func2();
-}
-
 int func2(float f)
 {
   return 1;
 }
 
+void func1(int x)
+{
+  int i = 1;
+  float z = 0.1;
+  float y = 0.2;
+  func2(0.1);
+  func2(0.2);
+}
+
 int main()
 {
-  func2(a,asdf);
-  func1( 3 , 2 );
+  func2(1.0);
+  func1(3);
 }
